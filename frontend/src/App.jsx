11067b3a10f729761ccfa92b4aee018e56042e0f
spunsosig <ikram15@live.co.uk>
2025-04-11 18:14:58 +0100

    Implemented side navigation bar with animation toggle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from 'react';
 import Header from "./components/Header";
 import Expertise from "./components/Expertise";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Work from "./components/Work";
-
+import SideNav from "./components/SideNav";
 
 function App() {
 
+  const homePageRef = useRef();
+  const [isHomePageVisible, setIsHomePageVisible] = useState();
+  console.log('is visible: ', isHomePageVisible);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setIsHomePageVisible(entry.isIntersecting);
+    })
+    observer.observe(homePageRef.current);
+  }, [])
+
   return(
     <>
-    <div className="flex flex-col justify-center">
-      <Header/>
-      <Hero></Hero>
-    </div>
+      <div ref={homePageRef} className="flex flex-col justify-center">
+        <Header/>
+        <Hero/>
+        <SideNav visible={!isHomePageVisible} />
+      </div>
       <div className="sections">
         <Expertise></Expertise>
         <Work></Work>
---
+++ frontend/src/components/SideNav.jsx
@@ -0,0 +1,19 @@
+import {Home, Glasses, Briefcase, FolderDot, Mail} from "lucide-react"
+
+const SideNav = ({ visible }) => {
+    return(
+        <nav
+            className={`fixed top-100 left-0 w-20 bg-red-900 flex flex-col items-center justify-center 
+            space-y-6 transition duration-300 ease-in-out transform 
+            ${visible ? "opacity-75 transition duration 300 hover:opacity-100 translate-x-0" : "opacity-0 -translate-x-5"}`}>
+
+            <a href="#home"><Home className="icon-style mt-5"></Home></a>
+            <a href="#expertise"><Glasses className="icon-style"></Glasses></a>
+            <a href="#work"><Briefcase className="icon-style"></Briefcase></a>
+            <a href="#projects"><FolderDot className="icon-style"></FolderDot></a>
+            <a href="#contact"><Mail className="icon-style mb-5"></Mail></a>
+        </nav>
+    );
+};
+
+export default SideNav;
