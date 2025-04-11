4fd957b627a0f91d09f6e353dacd01c508b72dd5
spunsosig <ikram15@live.co.uk>
2025-04-12 00:51:33 +0100

    Implemented project section no dropdown yet and changed fonts

--- frontend/src/components/Expertise.jsx
+++ frontend/src/components/Expertise.jsx
@@ -2,7 +2,7 @@ import Card from './Card';
 
 const Expertise = () => (
     <section id="expertise" className="page-section h-full">
-        <h2 className="section-heading">Expertise</h2>
+        <h2 className="section-heading font-extrabold">Expertise</h2>
         <div className='flex flex-col md:flex-row justify-center items-start gap-6'>
           <Card title="Software Development" description="Experienced in both functional and OOP: Dart, Python, Java, JavaScript, TypeScript."></Card>
           <Card title="Frontend Dev React, NextJS" description="Passionate about UI/UX. Over 5 years of development experience in HTML, CSS, JS, React and NextJS frameworks."></Card>
--- frontend/src/components/Work.jsx
+++ frontend/src/components/Work.jsx
@@ -1,9 +1,24 @@
+import { ChevronDown } from "lucide-react"
+
 const Work = () => (
     <section id="work" className="page-section">
-        <h2 className="section-heading">Work</h2>
-        <p>
-          Lorem ipsum dolor sit amet...
-        </p>
+        <h2 className="font-extrabold section-heading">Work</h2>
+        <div className="flex flex-col justify-center items-center">
+          <div className="w-3/6 flex flex-col rounded-xl justify-center items-start px-10 space-y-5 pt-5 pb-5">
+            <div className="work-dropdown">
+              <h1 className="work-headings">Burger King</h1>
+              <ChevronDown size={50}/>
+            </div>
+            <div className="work-dropdown">
+              <h1 className="work-headings">Mcdonalds</h1>
+              <ChevronDown size={50}/>
+            </div>
+            <div className="work-dropdown">
+              <h1 className="work-headings">Meta</h1>
+              <ChevronDown size={50}/>
+            </div>
+          </div>
+        </div>
     </section>
 )
 
