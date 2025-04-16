d8b08d1e3c0bef8a69d830665d23babcf39ef017
AGOrkhan <upperfloor2003@gmail.com>
2025-04-16 21:56:03 +0100

    Animated background

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,11 @@
 const Hero = () => {
     return (
-        <section id="home" className="page-section title-section">
+        <section id="home" className="page-section title-section relative min-h-screen flex items-center justify-center">
             <video autoPlay muted loop className="background-video">
                 <source src="assets/background.mp4" type="video/mp4" />
             </video>
             <p className="main-subtitle">Software Engineer, Full stack developer</p>
+            <div className="hero-bottom-gradient" />
         </section>
     );
 };
