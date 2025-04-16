4b9d532977de18d9552f0e57ccce772ad9f64bb2
Aorkhan <116641683+AGOrkhan@users.noreply.github.com>
2025-04-16 20:42:13 +0100

    Created the background animation to be used and container and css for it

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,12 @@
-const Hero = () => (
-    <section id="home" className="page-section title-section">
-
-          <h1 className="main-name-heading">TEST</h1>
-          <p className="main-subtitle">Software Engineer, Full stack developer</p>
-
-      </section>
-);
+const Hero = () => {
+    return (
+        <section id="home" className="page-section title-section">
+            <video autoPlay muted loop className="background-video">
+                <source src="assets/background.mp4" type="video/mp4" />
+            </video>
+            <p className="main-subtitle">Software Engineer, Full stack developer</p>
+        </section>
+    );
+};
 
 export default Hero;
