501aead8c3aec7454ec3057b336131c170a8ae40
spunsosig <ikram15@live.co.uk>
2025-04-17 22:23:44 +0100

    Made expertise page not overflow and added react router dom package

--- frontend/src/components/Expertise.jsx
+++ frontend/src/components/Expertise.jsx
@@ -9,7 +9,7 @@ const Expertise = () => {
 
     <section id="expertise" className="page-section h-full">
         <h2 className="section-heading font-extrabold">Expertise</h2>
-        <div className='flex flex-col pl-32 md:flex-row justify-center items-start gap-6'>
+        <div className='flex flex-col pl-32 lg:flex-row justify-center items-start gap-6'>
           {expertiseData.map((item, index) => (
             <div key={index} className="flex flex-col items-center">
                 <Card title={item.title} description={item.description} icon={<item.icon className="w-10 h-10 text-white mr-5" />} />
