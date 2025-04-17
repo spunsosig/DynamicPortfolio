f36fe9503812828048e22c8fe6ad7cb1b3f2bbb6
spunsosig <ikram15@live.co.uk>
2025-04-18 00:39:17 +0100

    Login and panel added to admin page which checks for authentication

--- frontend/src/AdminApp.jsx
+++ frontend/src/AdminApp.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import AdminLogin from './components/Admin/AdminLogin.jsx';
+import AdminPanel from './components/Admin/AdminPanel.jsx';
 
 function AdminApp() {
-  return (
-    <div className='min-h-screen bg-[#0b263a]'>
-        <AdminLogin/>
-    </div>
-  );
+
+    const [isAuthenticated, setIsAuthenticated] = useState(true)    
+
+    return (
+        <div className='min-h-screen bg-[#0b263a]'>
+            {!isAuthenticated && <AdminLogin onSuccess={() => setIsAuthenticated(true)} />}
+            {isAuthenticated && <AdminPanel />}
+        </div>
+    );
 }
 
 export default AdminApp;
---
+++ frontend/src/components/Admin/AdminPanel.jsx
@@ -0,0 +1,9 @@
+const AdminPanel = () => {
+    return (
+        <div className="min-h-screen text-white">
+            <h1 className="text-3xl font-bold">Admin Panel</h1>
+        </div>
+    );
+}
+
+export default AdminPanel
