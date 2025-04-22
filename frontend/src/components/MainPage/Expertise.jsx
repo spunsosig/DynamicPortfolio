5c68dc56865acad137d35ad5830a3af4d54d51b5
spunsosig <ikram15@live.co.uk>
2025-04-22 02:13:18 +0100

    Centered Expertise page for mobile

--- frontend/src/components/MainPage/Expertise.jsx
+++ frontend/src/components/MainPage/Expertise.jsx
@@ -9,9 +9,9 @@ const Expertise = () => {
 
     <section id="expertise" className="page-section h-full">
         <h2 className="section-heading font-extrabold">Expertise</h2>
-        <div className='flex flex-col pl-32 lg:flex-row justify-center items-start gap-6'>
+        <div className='flex flex-col lg:flex-row justify-center items-start gap-6 w-full'>
           {expertiseData.map((item, index) => (
-            <div key={index} className="flex flex-col items-center">
+            <div key={index} className="flex flex-col mx-auto lg:mx-0 items-center max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-sm">
                 <Card title={item.title} description={item.description} icon={<item.icon className="w-10 h-10 text-white mr-5" />} />
             </div>
           ))}
